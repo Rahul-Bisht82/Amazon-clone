7fcad1c47a64c27ae49fd684cec48292bf237171
rahul bisht <rahul.b4749@gmail.com>
2020-08-07 13:36:56 +0530

    Updates

---
+++ precache-manifest.552fb98567f3be563126cf1a6fb00bf7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a496bbf7d1f8f5669447c74dbb3e2cf",
+    "url": "/Amazon-clone/index.html"
+  },
+  {
+    "revision": "f213763f32663508691f",
+    "url": "/Amazon-clone/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "83c82ab47ba5c7489830",
+    "url": "/Amazon-clone/static/css/main.a1712691.chunk.css"
+  },
+  {
+    "revision": "f213763f32663508691f",
+    "url": "/Amazon-clone/static/js/2.787c3a9f.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/Amazon-clone/static/js/2.787c3a9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83c82ab47ba5c7489830",
+    "url": "/Amazon-clone/static/js/main.40b50780.chunk.js"
+  },
+  {
+    "revision": "b4ca44839ab564cdfff3",
+    "url": "/Amazon-clone/static/js/runtime-main.a692d69b.js"
+  }
+]);
